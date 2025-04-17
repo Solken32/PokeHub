a9cc94deddfa31541801a4d84eb9db33a7dbab03
solken32 <solistoke@gmail.com>
2025-04-16 21:43:47 -0500

    uploading files

---
+++ src/interfaces/pokemon-list-card.ts
@@ -0,0 +1,12 @@
+export interface PokemonListCard {
+[x: string]: any;
+    count:    number;
+    next:     string;
+    previous: null;
+    results:  Result[];
+}
+
+export interface Result {
+    name: string;
+    url:  string;
+}
